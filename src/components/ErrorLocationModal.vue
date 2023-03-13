6a90a37658941d096ecba183d915f53fafdd7e4e
flightoffancy-b <petuhov.ilja2015@yandex.ru>
2023-03-14 02:13:54 +0400

    first commit

---
+++ src/components/ErrorLocationModal.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div id="myModal" class="modal">
+        <RouterLink to="/"><span class="close">&times;</span></RouterLink>
+        <div class="modal-content">
+            <p class="text">Не получается определить <br> геопозицию, <i style="background: #E8E8E8; font-family: cursive;">Bastard</i>.
+                <br><br>Включи по братски<br>Настройки&#9658;Конфиденциальность и безопасность&#9658;Служба геолокации&#9658;Сайты Safari</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .modal {
+        width: 70vw;
+        height: 25vh;
+        background: #E8E8E8;
+        margin: 200px auto;
+        border-radius: 10px;
+        padding-top: 10px;
+        border: 1px solid gray;
+        box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+
+        .close {
+            border-radius: 10px;
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+            margin-right: 10px;
+            margin-top: -5px;
+            background: #E8E8E8;
+        }
+
+        .text {
+            font-weight: 400;
+            text-align: center;
+            padding: 15% 5px 0 5px;
+            background: #E8E8E8;
+            line-height: 1.2;
+            word-break: break-all;
+        }
+    }
+</style>
---
+++ src/views/MapContentView.vue
@@ -0,0 +1,140 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+import colorIzhToEnd from '../colorIzhevskToEnd.json'
+import colorMscToIzh from '../colorMoscowToIzhevsk.json'
+import modalErrorWindow from '../components/ErrorLocationModal.vue';
+
+//state
+const colorsLevelMsc = ref(colorMscToIzh) // array all color
+const colorsLevelIzh = ref(colorIzhToEnd)
+const modalError = ref(false)
+const radiusIzh = 20;
+const currDistanse = ref(-1)
+const distanseIzhToMsc = 969; // maybe other value
+const latitudEnd = 56.920559;
+const longitudeEnd = 53.270195;
+const color = ref('')
+
+//methods
+// Get position of the device, that using your site
+onMounted(async() => {
+    const success = (position) => {
+        modalError.value = false;
+        if (currDistanse.value == distance(position.coords.latitude, position.coords.longitude, latitudEnd, longitudeEnd)) {
+            return;
+        }
+
+        currDistanse.value = distance(position.coords.latitude, position.coords.longitude, latitudEnd, longitudeEnd);
+        if (currDistanse.value > radiusIzh) {
+            const indexColor = Math.round(currDistanse.value / (distanseIzhToMsc / colorsLevelMsc.value.length))
+            color.value = colorsLevelMsc.value[indexColor];
+        }
+        else {
+            const indexColor = Math.round(currDistanse.value / (radiusIzh / colorsLevelIzh.value.length))
+            color.value = colorsLevelIzh.value[indexColor];
+        }
+    }
+
+    const error = () => {
+        if (currDistanse.value == -1)
+            modalError.value = true
+    }
+
+    navigator.geolocation.watchPosition(success, error, { maximumAge: 0, timeout: 300, enableHighAccuracy: true })
+})
+
+// Get from degrees to radians
+const convertDeg2rad = (num: number) => {
+    return num * Math.PI / 180;
+}
+
+// Get distanse between two coordinates to kilometers.
+const distance = (lat_2: number, lon_2: number, lat_1: number, lon_1: number) => {
+
+    const radius_earth = 6371.1; // Радиус Земли
+
+    const lat_1R = convertDeg2rad(lat_1),
+        lon_1R = convertDeg2rad(lon_1),
+        lat_2R = convertDeg2rad(lat_2),
+        lon_2R = convertDeg2rad(lon_2);
+
+    const d = 2 * radius_earth * Math.asin(Math.sqrt(Math.sin((lat_2R - lat_1R) / 2) ** 2 + Math.cos(lat_1R) * Math.cos(lat_2R) * Math.sin((lon_2R - lon_1R) / 2) ** 2));
+
+    return Math.round(d * 1000) / 1000; // round to 3 number after comma
+}
+
+const getColorBar = (coef: number) => {
+    let rgb = hexToRgb(color.value);
+    let r = Number(rgb?.r) <= coef ? Number(rgb?.r) : Number(rgb?.r) - coef
+    let g = Number(rgb?.g) <= coef ? Number(rgb?.g) : Number(rgb?.g) - coef
+    let b = Number(rgb?.b) <= coef ? Number(rgb?.b) : Number(rgb?.b) - coef
+    rgb = { r: r, g: g, b: b, }
+    return rgbToHex(rgb.r, rgb.g, rgb.b);
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+    } : null;
+}
+
+</script>
+
+<template>
+    <main class="main" :style="{ background: color }">
+        <nav class="navbar" :style="{ background: getColorBar(20) }">
+            <div class="container flex" :style="{ background: getColorBar(20) }">
+                <RouterLink to="/" class="rl">
+                    <h2 class="link-title" :style="{ background: getColorBar(20) }">Shmavgeniy</h2>
+                </RouterLink>
+            </div>
+        </nav>
+        <modalErrorWindow v-if="modalError" />
+        <div v-if="!modalError" class="rectangle" :style="{ background: getColorBar(20) }">
+            <p class="distance__map-block" :style="{ background: getColorBar(20) }">{{ currDistanse >= 1 ? currDistanse + 'km' : currDistanse==0? '	&#129313;': currDistanse * 1000 + ' m'}}</p>
+        </div>
+    </main>
+</template> 
+
+<style scoped lang="scss">
+.main {
+    width: 100vw;
+    height: 100vh;
+
+    .navbar {
+        border-bottom: 1px solid #000;
+    }
+
+    .rectangle {
+        width: 300px;
+        height: 300px;
+        border: 1px solid black;
+        border-radius: 50%;
+        box-shadow: 0px 5px 10px 2px rgba(34, 60, 80, 0.2);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 100px auto;
+
+        .distance__map-block {
+            font-family: 'Montserrat', sans-serif;
+            font-style: normal;
+            font-weight: 300;
+            font-size: 50px;
+            position: relative;
+        }
+    }
+}
+</style>
